ee34a092501d888e9de56ef9b5c80f6a39465db0
agnieszka-miszkurka <miszkur@student.agh.edu.pl>
2017-10-28 15:58:48 +0200

    commit agi

--- app/src/main/java/com/example/admin/student/LoginActivity.java
+++ app/src/main/java/com/example/admin/student/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.admin.student;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -33,6 +34,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static android.Manifest.permission.READ_CONTACTS;
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 /**
  * A login screen that offers login via email/password.
@@ -333,7 +335,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                finish();
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
